b46f1d98e835673698ee8f3f315ad56fa96d7770
carlos vidal <carlospatriciovidal.c@gmail.com>
2023-10-09 20:40:51 -0300

    4to, Se completa vistas departamentos, tours, se modifican urls

--- TurismoReal/urls.py
+++ TurismoReal/urls.py
@@ -22,51 +22,61 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #index
-    path('inicio/',TurismoRealApp.views.index, name='index'),
-    path( '',TurismoRealApp.views.index, name='index'),
-    path( 'index/',TurismoRealApp.views.index, name='index'),
+
+    #HOME  PAGE
+    path('inicio/',TurismoRealApp.views.galeria_departamentos, name='index'),
+    path( '',TurismoRealApp.views.galeria_departamentos, name='galeria_departamentos'),
+
+
+    #VISTAS DE CLIENTES
+    path( 'tours/',TurismoRealApp.views.galeria_tours, name='galeria_tours'),
+    path( 'traslados/',TurismoRealApp.views.traslado, name='traslado'),
+
+
+    ##quitar el de abajo 
+    path('departamento/<int:dpto_id>/', TurismoRealApp.views.detalle_departamento, name='detalle_departamento'),
+    
+
+    #INGRESAR USUARIO
+    path( 'accounts/', include('django.contrib.auth.urls')),
+    #REGISTRAR CUENTA DE USUARIO
+    path('register/', TurismoRealApp.views.register, name='register'),
+
+    #PERFIL DE USUARIO ADMIN Y NO ADMIN 
     path( 'perfil/',TurismoRealApp.views.perfil, name='perfil'),
     
-    #menu admin
+    #==========================================================================
+    #VISTAS DE ADMINISTRADOR
     path( 'panel_administracion/',TurismoRealApp.views.panel_administracion, name='panel_administracion'),#read
     #usuario
     path( 'listar_Usuarios/',TurismoRealApp.views.listar_usuarios, name='listar_Usuarios'),#read
     path('usuarios/<int:usuario_id>/eliminar/', TurismoRealApp.views.activaDesactiva_usuario, name='activa_desactiva_usuario'),
-    #crud traslado
+    #crud traslado admin
     path( 'crear_traslado/',TurismoRealApp.views.crear_traslado, name='crear_traslado'),#create
     path( 'listar_Traslado/',TurismoRealApp.views.listar_Traslado, name='listar_Traslado'),#read
     path('traslado/<int:id_traslado>/eliminar/', TurismoRealApp.views.activaDesactiva_traslado, name='activa_desactiva_traslado'),#boton estado
-    #crud tour
+    #crud tour admin
     path( 'crear_tours/',TurismoRealApp.views.crear_tours, name='crear_tours'),#create
     path( 'listar_Tours/',TurismoRealApp.views.listar_Tours, name='listar_Tours'),#read
     path('tours/<int:id_tour>/eliminar/', TurismoRealApp.views.activaDesactiva_tour, name='activa_desactiva_tour'),#boton estado
-    #crud dpto
+    #crud dpto admin
     path( 'crear_departamento/',TurismoRealApp.views.crearDepartamentos, name='crear_departamento'),#create
     path( 'listar_Dpto/',TurismoRealApp.views.listar_Dpto, name='listar_Dpto'),#read
     path('departamentos/<int:dpto_id>/eliminar/', TurismoRealApp.views.activaDesactiva_departamento, name='activa_desactiva_departamento'),#boton estado
-    #crud servicio
+    #crud servicio admin
     path( 'crear_servicio/',TurismoRealApp.views.crear_servicio, name='crear_servicio'),#create
     path( 'listar_servicios/',TurismoRealApp.views.listar_servicios, name='listar_servicios'),#read
     path('servicios/<int:servicio_id>/eliminar/', TurismoRealApp.views.activaDesactiva_servicio, name='activa_desactiva_servicio'),
     path('servicios/<int:servicio_id>/', TurismoRealApp.views.actualizar_servicio, name='actualizar_servicio'), #update
-    #crud reporte
+    #crud reporte admin
     path( 'listar_Reporte/',TurismoRealApp.views.listar_Reporte, name='listar_Reporte'),#read
     path( 'mostrar_reporte/',TurismoRealApp.views.mostrar_reporte, name='mostrar_reporte'),#read unitario generar automaticamente en pl
-    #crud equipamiento
+    #crud equipamiento admin 
     path( 'crear_Equipamiento/',TurismoRealApp.views.crear_equipamiento, name='crear_Equipamiento'),#create
     path( 'listar_Equipamiento/',TurismoRealApp.views.listar_equipamiento, name='listar_Equipamiento'),#read
     path('equipamiento/<int:id_equipamiento>/eliminar/', TurismoRealApp.views.activa_desactiva_equipamiento, name='activa_desactiva_equipamiento'),
-    #vista cliente
-    path( 'galeria_departamentos/',TurismoRealApp.views.galeria_departamentos, name='galeria_departamentos'),
-    path('departamento/<int:dpto_id>/', TurismoRealApp.views.detalle_departamento, name='detalle_departamento'),
 
-    path( 'traslado/',TurismoRealApp.views.traslado, name='traslado'),
-    path( 'galeria_tours/',TurismoRealApp.views.galeria_tours, name='galeria_tours'),
-    #login
-    path( 'accounts/', include('django.contrib.auth.urls')),
-    #registrar cuenta
-    path('register/', TurismoRealApp.views.register, name='register'),
+
     ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
